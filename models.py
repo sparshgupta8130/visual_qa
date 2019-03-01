f6a48ac9096a35b0f785da06f8a7a231fc4b0cac
Anmol Popli <anmol.ap020@gmail.com>
2019-02-28 18:40:15 -0800

    models.py populated

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from torchvision import models
 
 
 class JointEmbedModel(nn.Module):
@@ -12,6 +13,57 @@ class JointEmbedModel(nn.Module):
         if embeds is not None:
             self.embed.weight.data.copy_(torch.from_numpy(embeds))
 
+        self.lstm_hidden_size = 512
+        self.lstm_num_layers = 2
+        self.lstmq = nn.LSTM(input_size=self.embedding_dim, hidden_size=self.lstm_hidden_size, num_layers=self.lstm_num_layers, batch_first=True)
+        
+        self.lstm_transform = nn.Sequential(
+            nn.Linear(2*self.lstm_num_layers*self.lstm_hidden_size, 1024),
+            nn.Tanh())
+        # self.lstm_transform.apply(init_weights)
+
+        self.vggnet = models.vgg16(pretrained=True)
+        self.vggnet.classifier = self.vggnet.classifier[:-2]
+        for param in self.vggnet.parameters():
+            param.requires_grad = False
+        
+        self.vggnet_transform = nn.Sequential(
+            nn.Linear(4096, 1024),
+            nn.Tanh())
+        # self.vggnet_transform.apply(init_weights)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(1024, 1000),
+            nn.Tanh(),
+            nn.Dropout(p=0.5),
+            nn.Linear(1000, 1000),
+            nn.Tanh(),
+            nn.Dropout(p=0.5),
+            nn.Linear(1000, int(config['n_classes'])))
+        # self.mlp.apply(init_weights)
+
+
     def forward(self, images, q_idxs):
-        return torch.ones(images.shape[0])
-        # TODO: Complete forward pass of the model
+        ques_embedded = self.embed(q_idxs)
+
+        lstm_out, (hn, cn) = self.lstm_q(ques_embedded)
+        hn = hn.permute(1, 0, 2)
+        cn = cn.permute(1, 0, 2)
+        hn = hn.view(images.shape[0], self.lstm_hidden_size*self.lstm_num_layers)
+        cn = cn.view(images.shape[0], self.lstm_hidden_size*self.lstm_num_layers)
+
+        ques_embedding = torch.cat((hn,cn), 1)
+        ques_embedding_transformed = self.lstm_transform(ques_embedding)
+
+        imgs_embedding = self.vggnet(images)
+        imgs_embedding_transformed = self.vggnet_transform(imgs_embedding)
+
+        joint_embedding = ques_embedding_transformed*imgs_embedding_transformed
+
+        output = self.mlp(joint_embedding)
+
+        return output
+
+def init_weights(net):
+    if type(net) == nn.Linear():
+        torch_init.xavier_normal_(net.weight)
